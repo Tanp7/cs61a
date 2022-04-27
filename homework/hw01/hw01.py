7ebcd4e300d94213cd3c19ea1909450d415d64a3
Pz <tanp11210712@gmail.com>
2022-04-27 12:05:45 +0800

    add hw01

--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -51,8 +51,8 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
-    ans = 0
-    for i in range(n):
+    ans = 1
+    for i in range(2,n):
         if(n%i==0):
             ans=max(ans,i)
     return ans
